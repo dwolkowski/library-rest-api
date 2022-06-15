17309ac94eb4bf14ff935a9b7b41bf3df71dbbfe
dwolkowski <darek.wolkowski@gmail.com>
2022-06-15 21:38:33 +0200

    Initial commit, add base configuration files.

---
+++ src/main/java/pl/dsw/dwolkowski/config/Config.java
@@ -0,0 +1,23 @@
+package pl.dsw.dwolkowski.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Config {
+
+    @Bean
+    public Docket swaggerConfiguration(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/library/v1/**"))
+                .apis(RequestHandlerSelectors.basePackage("pl.dsw.dwolkowski.api"))
+                .build();
+    }
+}
