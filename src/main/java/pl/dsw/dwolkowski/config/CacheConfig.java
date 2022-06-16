8d670bce979aa5021b96d2b1b424a648c4039c69
dwolkowski <darek.wolkowski@gmail.com>
2022-06-16 23:38:19 +0200

    Add basic cache support.

---
+++ src/main/java/pl/dsw/dwolkowski/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package pl.dsw.dwolkowski.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
