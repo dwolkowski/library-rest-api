015df90d5139203eb30a801b211158748879afc2
dwolkowski <darek.wolkowski@gmail.com>
2022-06-17 23:39:39 +0200

    Disable authorization for the duration of development.

--- src/main/java/pl/dsw/dwolkowski/config/SecurityConfig.java
+++ src/main/java/pl/dsw/dwolkowski/config/SecurityConfig.java
@@ -2,10 +2,16 @@ package pl.dsw.dwolkowski.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // Temporarily disabled
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 }
